e5be87b3e8d52a9ec90ca2b88ab037e460608e13
Cunnwee <kelwinjr11@gmail.com>
2026-07-17 11:49:08 +0700

    add the image to the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -57,23 +58,33 @@ class DatabaseSeeder extends Seeder
         $desserts = Category::create(['name' => 'Desserts', 'station' => 'kitchen']);
 
         $items = [
-            [$starters, 'Spring Rolls', 4.50],
-            [$starters, 'Garlic Bread', 3.50],
-            [$mains, 'Grilled Chicken', 12.00],
-            [$mains, 'Beef Burger', 10.50],
-            [$mains, 'Margherita Pizza', 9.50],
-            [$drinks, 'Coca-Cola', 2.00],
-            [$drinks, 'Orange Juice', 2.50],
-            [$drinks, 'Mojito', 6.00],
-            [$desserts, 'Chocolate Cake', 5.00],
-            [$desserts, 'Ice Cream', 3.00],
+            [$starters, 'Spring Rolls', 4.50, 'spring-rolls.jpg'],
+            [$starters, 'Garlic Bread', 3.50, 'garlic-bread.jpg'],
+            [$mains, 'Grilled Chicken', 12.00, 'grilled-chicken.jpg'],
+            [$mains, 'Beef Burger', 10.50, 'beef-burger.jpg'],
+            [$mains, 'Margherita Pizza', 9.50, 'margherita-pizza.jpg'],
+            [$drinks, 'Coca-Cola', 2.00, 'coca-cola.jpg'],
+            [$drinks, 'Orange Juice', 2.50, 'orange-juice.jpg'],
+            [$drinks, 'Mojito', 6.00, 'mojito.jpg'],
+            [$desserts, 'Chocolate Cake', 5.00, 'chocolate-cake.jpg'],
+            [$desserts, 'Ice Cream', 3.00, 'ice-cream.jpg'],
         ];
 
-        foreach ($items as [$category, $name, $price]) {
+        foreach ($items as [$category, $name, $price, $imageFile]) {
+            $imagePath = null;
+            $sourcePath = database_path('seeders/images/'.$imageFile);
+
+            if (file_exists($sourcePath)) {
+                $storedName = uniqid().'_'.$imageFile;
+                Storage::disk('public')->put('menu-items/'.$storedName, file_get_contents($sourcePath));
+                $imagePath = 'menu-items/'.$storedName;
+            }
+
             MenuItem::create([
                 'category_id' => $category->id,
                 'name' => $name,
                 'price' => $price,
+                'image' => $imagePath,
                 'is_available' => true,
             ]);
         }
